7d8bd940aab5b41f2aa3f35c2df49ef2b8e57652
jovyinny <jovinemutelani@gmail.com>
2023-03-30 13:53:51 +0300

    edits: readme and app.py supports media

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import telegram
 from sarufi import Sarufi
 from dotenv import load_dotenv
 from telegram import (
-Update,
-InlineKeyboardButton, 
-InlineKeyboardMarkup,
-ReplyKeyboardRemove
+    Update,
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup,
+    ReplyKeyboardRemove
 )
 from telegram.ext import (
-  CommandHandler,
+    CommandHandler,
     MessageHandler,
     filters,
     CallbackContext,
@@ -22,7 +22,7 @@ from telegram.ext import (
 
 # load .env 
 load_dotenv()
-# Set up Sarufi
+# Set up Sarufi and get bot's name
 sarufi = Sarufi(os.environ["sarufi_username"], os.environ["sarufi_password"])
 bot_name=sarufi.get_bot(os.environ["sarufi_bot_id"]).name
 
@@ -30,6 +30,65 @@ bot_name=sarufi.get_bot(os.environ["sarufi_bot_id"]).name
 # initialize the logger
 logger = logging.getLogger()
 
+
+def get_buttons(data:dict,type:str):
+  buttons=[]
+  if type=="reply_button":
+    
+    for button in data.get("buttons"):
+      button_title=button.get("reply").get("title")
+      button_id=button.get("reply").get("id")
+      button_data=[InlineKeyboardButton(button_title,callback_data=button_id)]
+      buttons.append(button_data)
+    return buttons
+  
+  else:
+    
+    for menu in data.get("sections")[0].get("rows"):
+      menu_title=menu.get("title")
+      menu_id=menu.get("id")
+      menu_button=[InlineKeyboardButton(menu_title,callback_data=menu_id)]
+      buttons.append(menu_button)
+    return buttons
+
+
+def get_clicked_button_text(buttons:tuple,button_callback_data:str)-> str:
+  for button in buttons:
+      if button[0].callback_data==button_callback_data:
+        return button[0].text
+
+
+async def send_medias(update: Update,context: CallbackContext,media:dict,type:str):
+  chat_id=update.effective_chat.id
+  
+  for file in media:
+    url=file.get("link")
+    caption=file.get("caption")
+    
+    if type=="images":
+      response = requests.get(url)
+      await context.bot.send_photo(chat_id=chat_id, photo=response.content,caption=caption)
+
+    elif type=="audios":
+      response = requests.get(url)
+      await context.bot.send_audi(chat_id=chat_id, audio=response.content,caption=caption)
+    
+    elif type=="videos":
+      response = requests.get(url)
+      await context.bot.send_video(chat_id=chat_id, video=response.content,caption=caption)
+    
+
+    elif type=="documents":
+      response = requests.get(url)
+      await context.bot.send_document(chat_id=chat_id, document=response.content,caption=caption)
+
+
+    elif type=="stickers":
+      response = requests.get(url)
+      await context.bot.send_stickers(chat_id=chat_id, sticker=response.content,caption=caption)
+
+    else:
+      logging.error(f"Sorry unrecognized media {type}")
 
 async def respond(message, chat_id,message_type="text")->dict:
     """
@@ -46,26 +105,12 @@ async def simulate_typing(update: Update, context: CallbackContext):
     )
     time.sleep(0.3)
 
-def get_buttons(data:dict,type:str):
-  buttons=[]
-  if type=="reply_button":
-    for button in data.get("buttons"):
-      button_title=button.get("reply").get("title")
-      button_id=button.get("reply").get("id")
-      button_data=[InlineKeyboardButton(button_title,callback_data=button_id)]
-      buttons.append(button_data)
-    return buttons
-  else:
-    for menu in data.get("sections")[0].get("rows"):
-      menu_title=menu.get("title")
-      menu_id=menu.get("id")
-      menu_button=[InlineKeyboardButton(menu_title,callback_data=menu_id)]
-      buttons.append(menu_button)
-    return buttons
 
 async def reply_with_typing(update: Update, context: CallbackContext, message):
+    
     await simulate_typing(update, context)
     chat_id=update.effective_chat.id
+    
     if isinstance(message,dict) or isinstance(message,list):
 
         for action in message:
@@ -92,14 +137,32 @@ async def reply_with_typing(update: Update, context: CallbackContext, message):
 
                 await context.bot.send_message(chat_id=chat_id, text=message,reply_markup=markdown)
 
+            elif action.get("send_images"):
+              images=action.get("send_images")
+              await send_medias(update,context,images,"images")
+
+
+            elif action.get("send_videos"):
+              videos=action.get("send_videos")
+              await send_medias(update,context,videos,"videos")
+
+            elif action.get("send_audios"):
+              audios=action.get("send_audios")
+              await send_medias(update,context,audios,"audios")
+
+            elif action.get("send_documents"):
+              documents=action.get("send_documents")
+              await send_medias(update,context,documents,"documents")
+
+            elif action.get("send_stickers"):
+              stickers=action.get("send_stickers")
+              await send_medias(update,context,stickers,"stickers")
+
+            else:
+                logger.error("Unkown action")
+
     else:
         await context.bot.send_message(chat_id=chat_id, text=message)
-
-
-def get_clicked_button_text(buttons:tuple,button_callback_data:str)-> str:
-  for button in buttons:
-      if button[0].callback_data==button_callback_data:
-        return button[0].text
 
 
 # handlers
